009fb69538e368b7be6bfe03dd7a09401a87e63b
aletovvladimir <72245011+aletovvladimir@users.noreply.github.com>
2020-12-17 22:56:50 +0300

    Update main.py

--- Lab 10 Final project/main.py
+++ Lab 10 Final project/main.py
@@ -1,26 +1,19 @@
-import time
-from math import sin, cos, asin, pi
-
 import pygame
-from pygame.draw import *
 
-from my_colors import *
+from my_variables import *
 from pendulum import *
 from menu import *
 from pendulum_graph import *
 from simulator import *
 
 pygame.init()
-
-FPS = 60
-SCREEN_X, SCREEN_Y = 1300, 600
-GROUND_Y = 19 * SCREEN_Y // 20
-
 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 
 if __name__ == "__main__":
     try:
         simulator = Simulator()
         simulator.mainloop()
+        
     finally:
         pygame.quit()
+
