2235d0f36ef166d004ef8deee43dc67634c1eede
aletovvladimir <72245011+aletovvladimir@users.noreply.github.com>
2020-12-17 23:00:10 +0300

    Update pendulum_graph.py

--- Lab 10 Final project/pendulum_graph.py
+++ Lab 10 Final project/pendulum_graph.py
@@ -1,16 +1,10 @@
 import pygame
 from pygame.draw import *
 
-from my_colors import *
+from my_variables import *
 from pendulum import *
-from menu import *
 
 pygame.init()
-
-FPS = 60
-SCREEN_X, SCREEN_Y = 1300, 600
-GROUND_Y = 19 * SCREEN_Y // 20
-
 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 
 class PendulumGraph:
@@ -64,4 +58,3 @@ class PendulumGraph:
                 SCREEN_Y / 3 - self._p.h
             )
         )
-
